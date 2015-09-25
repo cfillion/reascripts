a17218d697580415dca7ad388a8b575624322249
cfillion <cfillion@users.noreply.github.com>
2015-09-24 20:07:54 -0400

    don't require a valid selection to work

--- send_track.lua
+++ send_track.lua
@@ -1,17 +1,9 @@
 -- send_track.lua v0.1 by Christian Fillion (cfillion)
 
-local selectionSize = reaper.CountSelectedTracks(0)
-
-if selectionSize < 1 then
-  return reaper.ShowMessageBox(
-    "No tracks are selected.\nSelect at least one track and retry.",
-    "send_track.lua by cfillion", 0
-  )
-end
-
 reaper.PreventUIRefresh(1)
 reaper.Undo_BeginBlock()
 
+local selectionSize = reaper.CountSelectedTracks(0)
 local insertPos, index = reaper.GetNumTracks(), 0
 reaper.InsertTrackAtIndex(insertPos, true)
 local track = reaper.GetTrack(0, insertPos)
