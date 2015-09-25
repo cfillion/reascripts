b3998cc458624179348c70d09587b4a7942b0d42
cfillion <cfillion@users.noreply.github.com>
2015-09-24 20:01:09 -0400

    enhance the selection behavior of send_track.lua

--- send_track.lua
+++ send_track.lua
@@ -1,17 +1,8 @@
 -- send_track.lua v0.1 by Christian Fillion (cfillion)
 
-function GetInsertionPoint()
-  if g_selectionSize == 0 then
-    return reaper.GetNumTracks()
-  end
+local selectionSize = reaper.CountSelectedTracks(0)
 
-  track = reaper.GetSelectedTrack(0, g_selectionSize - 1)
-  return reaper.GetMediaTrackInfo_Value(track, "IP_TRACKNUMBER")
-end
-
-g_selectionSize = reaper.CountSelectedTracks(0)
-
-if g_selectionSize < 1 then
+if selectionSize < 1 then
   return reaper.ShowMessageBox(
     "No tracks are selected.\nSelect at least one track and retry.",
     "send_track.lua by cfillion", 0
@@ -21,12 +12,11 @@ end
 reaper.PreventUIRefresh(1)
 reaper.Undo_BeginBlock()
 
-local insertPos, index = GetInsertionPoint(), 0
-
+local insertPos, index = reaper.GetNumTracks(), 0
 reaper.InsertTrackAtIndex(insertPos, true)
 local track = reaper.GetTrack(0, insertPos)
 
-while index < g_selectionSize do
+while index < selectionSize do
   reaper.SNM_AddReceive(track, reaper.GetSelectedTrack(0, index), 0)
   reaper.BR_GetSetTrackSendInfo(
     track, 0, index, "I_SRCCHAN", true, 0)
@@ -38,7 +28,7 @@ while index < g_selectionSize do
   index = index + 1
 end
 
--- reaper.SetOnlyTrackSelected(track)
+reaper.SetOnlyTrackSelected(track)
 
 reaper.Undo_EndBlock("Create Send Track", 1)
 
