5758a6192cd4675c3c7f2b82cdb40a36fda09085
cfillion <cfillion@users.noreply.github.com>
2016-01-11 18:24:29 -0500

    song switcher: mouse press support

--- song_switcher.lua
+++ song_switcher.lua
@@ -70,7 +70,37 @@ function useColor(color)
   gfx.b = color[3] / 255
 end
 
-function printName(song)
+function textLine(text, x, padding)
+  local w, h = gfx.measurestr(text)
+  local y = gfx.y
+
+  if x == nil then
+    x = math.max(0, (gfx.w - w) / 2)
+  end
+
+  local tx, ty = x, y
+
+  if padding ~= nil then
+    x = x - padding
+    w = w + (padding * 2)
+
+    ty = y + padding
+    h = h + (padding * 2)
+  end
+
+  local rect = {x=0, y=y, w=gfx.w, h=h}
+  return {text=text, rect=rect, tx=tx, ty=ty}
+end
+
+function drawTextLine(line)
+  gfx.x = line.tx
+  gfx.y = line.ty
+
+  gfx.drawstr(line.text)
+  gfx.y = line.ty + line.rect.h
+end
+
+function drawName(song)
   local name = "##. No Song Selected"
 
   if song ~= nil then
@@ -80,11 +110,32 @@ function printName(song)
   gfx.setfont(FONT_LARGE)
   useColor(COLOR_WHITE)
 
-  local w, h = gfx.measurestr(name)
-  gfx.x = math.max(0, (gfx.w - w) / 2)
+  drawTextLine(textLine(name))
+end
 
-  gfx.drawstr(name)
-  gfx.y = gfx.y + h
+function drawSongList()
+  gfx.setfont(FONT_DEFAULT)
+
+  local index = 0
+  local song = songs[index]
+
+  while song ~= nil do
+    local line = textLine(song.name, 10, PADDING)
+    local x, y, w, h = line.rect
+
+    if mouseState > 0 and isUnderMouse(line.rect.x, line.rect.y, line.rect.w, line.rect.h) then
+      useColor(COLOR_HIGHLIGHT)
+      gfx.rect(line.rect.x, line.rect.y, line.rect.w, line.rect.h)
+      useColor(COLOR_BLACK)
+    else
+      useColor(COLOR_LGRAY)
+    end
+
+    drawTextLine(line)
+
+    index = index + 1
+    song = songs[index]
+  end
 end
 
 function keyboard()
@@ -107,17 +158,50 @@ function keyboard()
   end
 end
 
+function isUnderMouse(x, y, w, h)
+  local hor, ver = false, false
+
+  if gfx.mouse_x > x and gfx.mouse_x < x + w then
+    hor = true
+  end
+
+  if gfx.mouse_y > y and gfx.mouse_y < y + h then
+    ver = true
+  end
+
+  if hor and ver then return true else return false end
+end
+
+function mouse()
+  if mouseState == 0 and gfx.mouse_cap ~= 0 then
+    -- NOTE: mouse press handling here
+  end
+
+  if mouseState == 1 and gfx.mouse_cap == 0 then
+    onClick()
+  end
+
+  mouseState = gfx.mouse_cap
+end
+
+function onClick()
+end
+
 function loop()
   gfx.y = 10
-  printName(songs[currentIndex])
+  drawName(songs[currentIndex])
 
   gfx.y = gfx.y + 10
-  useColor(COLOR_GRAY)
+  useColor(COLOR_DGRAY)
   gfx.line(0, gfx.y, gfx.w, gfx.y)
+
+  gfx.y = gfx.y + 10
+  drawSongList()
 
   gfx.update()
 
   keyboard()
+  mouse()
 end
 
 songs = loadTracks()
@@ -127,18 +211,26 @@ currentIndex = -1
 setCurrentIndex(-1)
 
 -- graphic initialization
+FONT_DEFAULT = 0
 FONT_LARGE = 1
 FONT_SMALL = 2
 
 COLOR_WHITE = {255, 255, 255}
-COLOR_GRAY = {178, 178, 178}
+COLOR_LGRAY = {200, 200, 200}
+COLOR_DGRAY = {178, 178, 178}
+COLOR_HIGHLIGHT = {164, 204, 255}
+COLOR_BLACK = {0, 0, 0}
 
 KEY_ESCAPE = 27
 KEY_SPACE = 32
 
+PADDING = 3
+
+mouseState = 0
+
 gfx.init("cfillion's Song Switcher", 500, 300)
 gfx.setfont(FONT_LARGE, "sans-serif", 28, 'b')
-gfx.setfont(FONT_SMALL, "sans-serif", 10)
+gfx.setfont(FONT_SMALL, "sans-serif", 13)
 
 -- GO!!
 reaper.defer(loop)
