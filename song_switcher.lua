cfillion/reascripts

commit 695c81a269cdc1ffb22b8d97fc88d8cecbb9dc7c

song switcher: reset on three-button click

song_switcher.lua
@@ -463,11 +463,15 @@ function mouse()
     -- NOTE: mouse press handling here
   end
 
-  if mouseState == 1 and gfx.mouse_cap == 0 then
-    -- mouse release
-    mouseClick = true
+  if mouseState == 3 and gfx.mouse_cap < 3 and gfx.mouse_cap >= 0 then
+    -- two button release
+    -- also triggered if one button is released slightly before the other
+    reset()
+  elseif mouseState == 1 and gfx.mouse_cap == 0 then
+    -- left button releace
+
     local now = os.clock()
-    if lastClick > now - 0.1 then
+    if lastClick > now - 0.2 then
       isDoubleClick = true
       lastClick = 0
     else

commit c194a0b66cbd42d4d1fe164affc0910603ad033a

song switcher: fix left button click which was broken since 695c81a269cdc1ffb22b8d97fc88d8cecbb9dc7c

song_switcher.lua
@@ -451,6 +451,7 @@ end
 
 function mouse()
   isDoubleClick = false
+  mouseClick = false
 
   if gfx.mouse_wheel ~= 0 then
     local offset = math.max(0, scrollOffset - gfx.mouse_wheel)
@@ -468,7 +469,7 @@ function mouse()
     -- also triggered if one button is released slightly before the other
     reset()
   elseif mouseState == 1 and gfx.mouse_cap == 0 then
-    -- left button releace
+    -- left button release
 
     local now = os.clock()
     if lastClick > now - 0.2 then
@@ -477,8 +478,8 @@ function mouse()
     else
       lastClick = now
     end
-  else
-    mouseClick = false
+
+    mouseClick = true
   end
 
   mouseState = gfx.mouse_cap