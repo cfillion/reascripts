461de832063673c46a87ccfab2b529ec0e1946b9
cfillion <cfillion@users.noreply.github.com>
2016-03-04 01:35:43 -0500

    explode track to mono tracks: bug fixes and explicit REAPER 5.16 requirement (v1.1)

--- explode_track_to_mono_tracks.lua
+++ explode_track_to_mono_tracks.lua
@@ -1,5 +1,6 @@
--- cfillion_Explode track to mono tracks.lua
--- @version 1.0
+-- cfillion_Explode track to mono tracks.lua for REAPER v5.16+
+-- http://forum.cockos.com/showthread.php?p=1647321
+-- @version 1.1
 -- @author cfillion
 
 local tracks = reaper.CountSelectedTracks(0)
@@ -14,20 +15,21 @@ reaper.Undo_BeginBlock()
 
 for trackIndex=0,tracks-1 do
   local parent = reaper.GetSelectedTrack(0, trackIndex)
-  local chans = reaper.GetMediaTrackInfo_Value(parent, "I_NCHAN", channels)
+  local chans = reaper.GetMediaTrackInfo_Value(parent, "I_NCHAN")
+  local trackId = reaper.GetMediaTrackInfo_Value(parent, "IP_TRACKNUMBER")
   local _, name = reaper.GetSetMediaTrackInfo_String(parent, "P_NAME", "", false)
 
   reaper.SetMediaTrackInfo_Value(parent, "B_MAINSEND", 0)
 
   for chanIndex=0,chans-1 do
-    local insertIndex = trackIndex + chanIndex + 1
+    local insertIndex = trackId + chanIndex
     reaper.InsertTrackAtIndex(insertIndex, true)
     track = reaper.GetTrack(0, insertIndex)
 
     local send = reaper.CreateTrackSend(parent, track)
     reaper.SetTrackSendInfo_Value(parent, 0, send, "I_SRCCHAN", chanIndex | 1024)
     reaper.GetSetMediaTrackInfo_String(track, "P_NAME",
-      string.format("Ch. #%02d - %s", chanIndex, name), true)
+      string.format("Ch. %d - %s", chanIndex + 1, name), true)
   end
 end
 
