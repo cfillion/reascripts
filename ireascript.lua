6a7663b507a8831db759572310bfb59440b8fba7
cfillion <cfillion@users.noreply.github.com>
2016-05-21 17:49:12 -0400

    ireascript: don't expose private variables and functions globally

--- ireascript.lua
+++ ireascript.lua
@@ -1,26 +1,62 @@
-function reset(banner)
-  buffer = {}
-  wrappedBuffer = {w = 0}
-  input = ''
+local ireascript = {
+  TITLE = 'Interactive ReaScript',
+  BANNER = 'Interactive ReaScript v1.0 by cfillion',
+  MARGIN = 3,
 
-  resetFormat()
+  BUILTIN = {
+    help = function()
+      ireascript.push("help")
+    end,
+    clear = function()
+      ireascript.reset(false)
+      ireascript.update()
+    end,
+  },
+
+  FONT_NORMAL = 1,
+  FONT_BOLD = 2,
+
+  COLOR_DEFAULT = {190, 190, 190},
+  COLOR_BLUE = {90, 90, 190},
+  COLOR_RED = {190, 90, 90},
+  COLOR_BLACK = {0, 0, 0},
+
+  SG_NEWLINE = 1,
+  SG_CURSOR = 2,
+
+  EXT_SECTION = 'cfillion_ireascripts',
+
+  KEY_BACKSPACE = 8,
+  KEY_CLEAR = 144,
+  KEY_CTRLU = 21,
+  KEY_ENTER = 13,
+  KEY_INPUTRANGE_FIRST = 32,
+  KEY_INPUTRANGE_LAST = 125,
+}
+
+function ireascript.reset(banner)
+  ireascript.buffer = {}
+  ireascript.wrappedBuffer = {w = 0}
+  ireascript.input = ''
+
+  ireascript.resetFormat()
 
   if banner then
-    push('Interactive ReaScript v0.1 by cfillion')
-    nl()
-    push("Type Lua code or 'help'")
-    nl()
+    ireascript.push('Interactive ReaScript v0.1 by cfillion')
+    ireascript.nl()
+    ireascript.push("Type Lua code or 'help'")
+    ireascript.nl()
   end
 
-  prompt()
+  ireascript.prompt()
 end
 
-function keyboard()
+function ireascript.keyboard()
   local char = gfx.getchar()
 
   if char < 0 then
     -- bye bye!
-    saveDockedState()
+    ireascript.saveDockedState()
     return false
   end
 
@@ -29,48 +65,48 @@ function keyboard()
   --   reaper.ShowConsoleMsg("\n")
   -- end
 
-  if char == KEY_BACKSPACE then
-    input = string.sub(input, 0, -2)
-    prompt()
-    update()
-  elseif char == KEY_CLEAR or char == KEY_CTRLU then
-    input = ''
-    prompt()
-    update()
-  elseif char == KEY_ENTER then
-    eval()
-  elseif char >= KEY_INPUTRANGE_FIRST and char <= KEY_INPUTRANGE_LAST then
-    input = input .. string.char(char)
-    prompt()
-    update()
+  if char == ireascript.KEY_BACKSPACE then
+    ireascript.input = string.sub(ireascript.input, 0, -2)
+    ireascript.prompt()
+    ireascript.update()
+  elseif char == ireascript.KEY_CLEAR or char == ireascript.KEY_CTRLU then
+    ireascript.input = ''
+    ireascript.prompt()
+    ireascript.update()
+  elseif char == ireascript.KEY_ENTER then
+    ireascript.eval()
+  elseif char >= ireascript.KEY_INPUTRANGE_FIRST and char <= ireascript.KEY_INPUTRANGE_LAST then
+    ireascript.input = ireascript.input .. string.char(char)
+    ireascript.prompt()
+    ireascript.update()
   end
 
   return true
 end
 
-function draw()
-  gfx.x = MARGIN
-  gfx.y = MARGIN
+function ireascript.draw()
+  gfx.x = ireascript.MARGIN
+  gfx.y = ireascript.MARGIN
 
   local height = 0
 
-  for i=1,#wrappedBuffer do
-    local segment = wrappedBuffer[i]
+  for i=1,#ireascript.wrappedBuffer do
+    local segment = ireascript.wrappedBuffer[i]
 
-    if segment == SG_NEWLINE then
-      gfx.x = MARGIN
+    if segment == ireascript.SG_NEWLINE then
+      gfx.x = ireascript.MARGIN
       gfx.y = gfx.y + height
-    elseif segment == SG_CURSOR then
+    elseif segment == ireascript.SG_CURSOR then
       if os.time() % 2 == 0 then
         gfx.line(gfx.x, gfx.y, gfx.x, gfx.y + height)
       end
     else
       gfx.setfont(segment.font)
 
-      useColor(segment.bg)
+      ireascript.useColor(segment.bg)
       gfx.rect(gfx.x, gfx.y, segment.w, segment.h)
 
-      useColor(segment.fg)
+      ireascript.useColor(segment.fg)
 
       gfx.drawstr(segment.text)
       height = math.max(height, segment.h)
@@ -78,26 +114,26 @@ function draw()
   end
 end
 
-function update()
-  wrappedBuffer = {}
-  wrappedBuffer.w = gfx.w
+function ireascript.update()
+  ireascript.wrappedBuffer = {}
+  ireascript.wrappedBuffer.w = gfx.w
 
-  local leftmost = MARGIN
+  local leftmost = ireascript.MARGIN
   local left = leftmost
 
-  for i=1,#buffer do
-    local segment = buffer[i]
+  for i=1,#ireascript.buffer do
+    segment = ireascript.buffer[i]
 
     if type(segment) ~= 'table' then
-      wrappedBuffer[#wrappedBuffer + 1] = segment
+      ireascript.wrappedBuffer[#ireascript.wrappedBuffer + 1] = segment
 
-      if segment == SG_NEWLINE then
+      if segment == ireascript.SG_NEWLINE then
         left = leftmost
       end
     else
       gfx.setfont(segment.font)
 
-      text = segment.text
+      local text = segment.text
 
       while text:len() > 0 do
         local w, h = gfx.measurestr(text)
@@ -112,14 +148,14 @@ function update()
 
         left = left + w
 
-        local newSeg = dup(segment)
+        local newSeg = ireascript.dup(segment)
         newSeg.text = text:sub(0, count)
         newSeg.w = w
         newSeg.h = h
-        wrappedBuffer[#wrappedBuffer + 1] = newSeg
+        ireascript.wrappedBuffer[#ireascript.wrappedBuffer + 1] = newSeg
 
         if resized then
-          wrappedBuffer[#wrappedBuffer + 1] = SG_NEWLINE
+          ireascript.wrappedBuffer[#ireascript.wrappedBuffer + 1] = ireascript.SG_NEWLINE
           left = leftmost
         end
 
@@ -129,118 +165,126 @@ function update()
   end
 end
 
-function loop()
-  if keyboard() then
-    reaper.defer(loop)
+function ireascript.loop()
+  if ireascript.keyboard() then
+    reaper.defer(ireascript.loop)
   end
 
-  if wrappedBuffer.w ~= gfx.w then
-    update()
+  if ireascript.wrappedBuffer.w ~= gfx.w then
+    ireascript.update()
   end
 
-  draw()
+  ireascript.draw()
 
   gfx.update()
 end
 
-function resetFormat()
-  font = FONT_NORMAL
-  foreground = COLOR_DEFAULT
-  background = COLOR_BLACK
+function ireascript.resetFormat()
+  ireascript.font = ireascript.FONT_NORMAL
+  ireascript.foreground = ireascript.COLOR_DEFAULT
+  ireascript.background = ireascript.COLOR_BLACK
 end
 
-function errorFormat()
-  font = FONT_BOLD
-  foreground = COLOR_DEFAULT
-  background = COLOR_RED
+function ireascript.errorFormat()
+  ireascript.font = ireascript.FONT_BOLD
+  ireascript.foreground = ireascript.COLOR_DEFAULT
+  ireascript.background = ireascript.COLOR_RED
 end
 
-function nl()
-  buffer[#buffer + 1] = SG_NEWLINE
+function ireascript.nl()
+  ireascript.buffer[#ireascript.buffer + 1] = ireascript.SG_NEWLINE
 end
 
-function push(contents)
-  buffer[#buffer + 1] = {font=font, fg=foreground, bg=background, text=contents}
+function ireascript.push(contents)
+  if contents == nil then
+    error('content is nil')
+  end
+
+  ireascript.buffer[#ireascript.buffer + 1] = {
+    font=ireascript.font,
+    fg=ireascript.foreground, bg=ireascript.background,
+    text=contents,
+  }
 end
 
-function prompt()
-  resetFormat()
-  backtrack()
-  push('> ')
-  push(input)
-  buffer[#buffer + 1] = SG_CURSOR
+function ireascript.prompt()
+  ireascript.resetFormat()
+  ireascript.backtrack()
+  ireascript.push('> ')
+  ireascript.push(ireascript.input)
+  ireascript.buffer[#ireascript.buffer + 1] = ireascript.SG_CURSOR
 end
 
-function backtrack()
-  local i = #buffer
+function ireascript.backtrack()
+  local i = #ireascript.buffer
   while i >= 1 do
-    if buffer[i] == SG_NEWLINE then
+    if ireascript.buffer[i] == ireascript.SG_NEWLINE then
       return
     end
 
-    table.remove(buffer)
+    table.remove(ireascript.buffer)
     i = i - 1
   end
 end
 
-function removeCursor()
-  local i = #buffer
+function ireascript.removeCursor()
+  local i = #ireascript.buffer
   while i >= 1 do
-    local segment = buffer[i]
+    local segment = ireascript.buffer[i]
 
-    if segment == SG_NEWLINE then
+    if segment == ireascript.SG_NEWLINE then
       return
-    elseif segment == SG_CURSOR then
-      table.remove(buffer)
+    elseif segment == ireascript.SG_CURSOR then
+      table.remove(ireascript.buffer)
     end
 
     i = i - 1
   end
 end
 
-function eval()
-  removeCursor()
-  nl()
+function ireascript.eval()
+  ireascript.removeCursor()
+  ireascript.nl()
 
-  local builtin = BUILTIN[input:lower()]
+  local builtin = ireascript.BUILTIN[ireascript.input:lower()]
 
   if builtin then
     builtin()
 
-    if input:len() == 0 then
+    if ireascript.input:len() == 0 then
       return -- buffer got reset
     end
 
-    nl()
-  elseif input:len() > 0 then
-    lua(input)
-    nl()
+    ireascript.nl()
+  elseif ireascript.input:len() > 0 then
+    ireascript.lua(ireascript.input)
+    ireascript.nl()
   end
 
-  input = ''
-  prompt()
-  update()
+  ireascript.input = ''
+  ireascript.prompt()
+  ireascript.update()
 end
 
-function lua(code)
+function ireascript.lua(code)
   local func, err = load('return ' .. code, 'eval')
 
   if err then
-    errorFormat()
-    push(err:sub(20))
+    ireascript.errorFormat()
+    ireascript.push(err:sub(20))
   else
     local values = {func()}
 
     if #values <= 1 then
-      format(values[1])
+      ireascript.format(values[1])
     else
-      format(values)
+      ireascript.format(values)
     end
   end
 end
 
-function format(value)
-  resetFormat()
+function ireascript.format(value)
+  ireascript.resetFormat()
 
   local t = type(value)
 
@@ -256,112 +300,81 @@ function format(value)
     end
 
     if array then
-      formatArray(value)
+      ireascript.formatArray(value)
     else
-      formatTable(value)
+      ireascript.formatTable(value)
     end
   else
-    push(tostring(value))
+    ireascript.push(tostring(value))
   end
 end
 
-function formatArray(value)
+function ireascript.formatArray(value)
   local i = 1
 
-  push('[')
+  ireascript.push('[')
   for k,v in ipairs(value) do
     if i > 1 then
-      push(', ')
+      ireascript.push(', ')
     end
 
-    format(v)
+    ireascript.format(v)
     i = i + 1
   end
-  push(']')
+  ireascript.push(']')
 end
 
-function formatTable(value)
+function ireascript.formatTable(value)
   local i = 1
 
-  push('{')
+  ireascript.push('{')
   for k,v in pairs(value) do
     if i > 1 then
-      push(', ')
+      ireascript.push(', ')
     end
 
-    format(k)
-    push(' = ')
-    format(v)
+    ireascript.format(k)
+    ireascript.push(' = ')
+    ireascript.format(v)
+
     i = i + 1
   end
-  push('}')
+  ireascript.push('}')
 end
 
-function useColor(color)
+function ireascript.useColor(color)
   gfx.r = color[1] / 255
   gfx.g = color[2] / 255
   gfx.b = color[3] / 255
 end
 
-function dup(table)
+function ireascript.dup(table)
   local copy = {}
   for k,v in pairs(table) do copy[k] = v end
   return copy
 end
 
-function restoreDockedState()
-  local docked_state = tonumber(reaper.GetExtState(EXT_SECTION, 'docked_state'))
+function ireascript.restoreDockedState()
+  local docked_state = tonumber(reaper.GetExtState(
+    ireascript.EXT_SECTION, 'docked_state'))
 
   if docked_state then
     gfx.dock(docked_state)
   end
 end
 
-function saveDockedState()
-  reaper.SetExtState(EXT_SECTION, 'docked_state', tostring(dockState), true)
+function ireascript.saveDockedState()
+  reaper.SetExtState(ireascript.EXT_SECTION,
+    'docked_state', tostring(dockState), true)
 end
 
-TITLE = 'Interactive ReaScript'
-BANNER = 'Interactive ReaScript v1.0 by cfillion'
-MARGIN = 3
+ireascript.reset(true)
 
-BUILTIN = {
-  help = function()
-    push("help")
-  end,
-  clear = function()
-    reset(false)
-    update()
-  end,
-}
+gfx.init(ireascript.TITLE, 500, 300)
+gfx.setfont(ireascript.FONT_NORMAL, 'Courier', 14)
+gfx.setfont(ireascript.FONT_BOLD, 'Courier', 14, 'b')
 
-FONT_NORMAL = 1
-FONT_BOLD = 2
-
-COLOR_DEFAULT = {190, 190, 190}
-COLOR_BLUE = {90, 90, 190}
-COLOR_RED = {190, 90, 90}
-COLOR_BLACK = {0, 0, 0}
-
-SG_NEWLINE = 1
-SG_CURSOR = 2
-
-EXT_SECTION = 'cfillion_ireascripts'
-
-KEY_BACKSPACE = 8
-KEY_CLEAR = 144
-KEY_CTRLU = 21
-KEY_ENTER = 13
-KEY_INPUTRANGE_FIRST = 32
-KEY_INPUTRANGE_LAST = 125
-
-reset(true)
-
-gfx.init(TITLE, 500, 300)
-gfx.setfont(FONT_NORMAL, 'Courier', 14)
-gfx.setfont(FONT_BOLD, 'Courier', 14, 'b')
-
-restoreDockedState()
+ireascript.restoreDockedState()
 
 -- GO!!
-loop()
+ireascript.loop()
