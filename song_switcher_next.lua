8407f9f0321ca753ef4e355ad64f3f54be3b2221
cfillion <cfillion@users.noreply.github.com>
2016-09-06 18:23:34 -0400

    song switcher: don't create undo point when using next/prev scripts

--- song_switcher_next.lua
+++ song_switcher_next.lua
@@ -1,1 +1,6 @@
-reaper.SetExtState('cfillion_song_switcher', 'relative_move', 1, false)
+-- This script is part of cfillion_Song Switcher.lua
+-- @noindex
+
+reaper.defer(function() -- no undo point
+  reaper.SetExtState('cfillion_song_switcher', 'relative_move', '1', false)
+end)
