baaf0cde7a923748abbf54070e9684fe3ec8f45c
cfillion <cfillion@users.noreply.github.com>
2018-01-18 10:50:18 -0500

    add action for (un)selecting automation items under mouse/edit cursor

--- cfillion_Automation item selection bundle.lua
+++ cfillion_Automation item selection bundle.lua
@@ -1,30 +1,43 @@
--- @description Automation item selection bundle (12 actions)
--- @version 1.0
+-- @description Automation item selection bundle
+-- @version 1.1
+-- @changelog
+--   Add actions for unselecting all automations items (+ in pool)
+--   Add actions selecting/adding to selection the automation item under edit and mouse cursor
 -- @author cfillion
 -- @provides
---   [main] . > cfillion_Select and move to next automation item.lua
---   [main] . > cfillion_Select and move to next automation item in pool.lua
---   [main] . > cfillion_Select and move to previous automation item.lua
---   [main] . > cfillion_Select and move to previous automation item in pool.lua
---   [main] . > cfillion_Add next automation item to selection.lua
---   [main] . > cfillion_Add next automation item in pool to selection.lua
---   [main] . > cfillion_Add previous automation item to selection.lua
---   [main] . > cfillion_Add previous automation item in pool to selection.lua
---   [main] . > cfillion_Select all automation items.lua
---   [main] . > cfillion_Select all automation items in pool.lua
---   [main] . > cfillion_Unelect all automation items.lua
---   [main] . > cfillion_Unelect all automation items in pool.lua
+--   . > cfillion_Select and move to next automation item.lua
+--   . > cfillion_Select and move to next automation item in pool.lua
+--   . > cfillion_Select and move to previous automation item.lua
+--   . > cfillion_Select and move to previous automation item in pool.lua
+--
+--   . > cfillion_Add next automation item to selection.lua
+--   . > cfillion_Add next automation item in pool to selection.lua
+--   . > cfillion_Add previous automation item to selection.lua
+--   . > cfillion_Add previous automation item in pool to selection.lua
+--   . > cfillion_Add all automation items under edit cursor to selection.lua
+--   . > cfillion_Add all automation items under mouse cursor to selection.lua
+--
+--   . > cfillion_Select all automation items.lua
+--   . > cfillion_Select all automation items in pool.lua
+--   . > cfillion_Select all automation items under edit cursor.lua
+--   . > cfillion_Select all automation items under mouse cursor.lua
+--
+--   . > cfillion_Unselect all automation items.lua
+--   . > cfillion_Unselect all automation items in pool.lua
+--   . > cfillion_Unselect all automation items under edit cursor.lua
+--   . > cfillion_Unselect all automation items under mouse cursor.lua
 -- @about
 --   # Automation item selection bundle
 --
---   This package provides a total of 12 actions for selecting or unselecting
+--   This package provides a total of 18 actions for selecting or unselecting
 --   automation items in the selected envelope lane. See the Contents tab for
 --   the list and for the exact name of the actions.
 --
 --   - Actions for selecting and moving to the next or previous AIs
 --   - Actions for preserving the current selection
 --   - Actions for cycling through the AIs in the selected pool
---   - Actions for selecting or unselecting all AIs or AIs in the selected pool
+--   - Actions for selecting or unselecting all AIs, AIs in the selected pool,
+--   under the edit cursor and under the mouse cursor
 -- @link
 --   cfillion's website https://cfillion.ca
 --   Original request https://github.com/reaper-oss/sws/issues/899
@@ -34,17 +47,33 @@ local UNDO_STATE_TRACKCFG = 1
 
 local name = ({reaper.get_action_context()})[2]:match("([^/\\_]+).lua$")
 
+local moveMode = name:match('move to')
 local poolMode = name:match('pool')
 local addToSelMode = name:match('Add.+to selection')
 local prevMode = name:match('previous')
 local entireBucketMode = name:match('all')
 local unselectMode = name:match('Unselect')
+local editCursorMode = name:match('under edit cursor')
+local mouseCursorMode = name:match('under mouse cursor')
 
--- poolMode = false
--- addToSelMode = false
--- prevMode = false
--- entireBucketMode = false
--- unselectMode = false
+function testCursorPosition(env, startTime, endTime)
+  local curPos
+
+  if editCursorMode then
+    curPos = reaper.GetCursorPosition()
+  elseif mouseCursorMode then
+    reaper.BR_GetMouseCursorContext()
+    if reaper.BR_GetMouseCursorContext_Envelope() == env then
+      curPos = reaper.BR_GetMouseCursorContext_Position()
+    else
+      return false
+    end
+  else
+    return true
+  end
+
+  return startTime <= curPos and endTime >= curPos
+end
 
 local env = reaper.GetSelectedEnvelope(0)
 if not env then
@@ -65,6 +94,8 @@ for i=0,count-1 do
   local selected = 1 == reaper.GetSetAutomationItemInfo(env, i, 'D_UISEL', 0, false)
   local bucketId = 0
   local startTime = reaper.GetSetAutomationItemInfo(env, i, 'D_POSITION', 0, false)
+  local length = reaper.GetSetAutomationItemInfo(env, i, 'D_LENGTH', 0, false)
+  local underCursor = testCursorPosition(env, startTime, startTime + length)
 
   if poolMode then
     bucketId = reaper.GetSetAutomationItemInfo(env, i, 'D_POOL_ID', 0, false)
@@ -78,7 +109,7 @@ for i=0,count-1 do
     table.insert(currentSel, i)
   end
 
-  if not selected or entireBucketMode then
+  if (not selected or entireBucketMode or unselectMode) and underCursor then
     local ai = {id=i, pos=startTime}
 
     if buckets[bucketId] then
@@ -135,7 +166,7 @@ if not addToSelMode and not unselectMode then
     reaper.GetSetAutomationItemInfo(env, ai, 'D_UISEL', 0, true)
   end
   
-  if not entireBucketMode then
+  if moveMode then
     reaper.SetEditCurPos(bucket[target].pos, true, false)
   end
 end
