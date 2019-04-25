2cf053ae69c6bdffa25924104faf63be47ed04a2
cfillion <cfillion@users.noreply.github.com>
2019-04-25 11:57:16 -0400

    update song swither's name's case in comments and in the web interface

--- song_switcher_next.lua
+++ song_switcher_next.lua
@@ -1,6 +1,6 @@
--- This script is part of cfillion_Song Switcher.lua
 -- @noindex
 
+-- This script is part of cfillion_Song switcher.lua
 reaper.defer(function() -- no undo point
   reaper.SetExtState('cfillion_song_switcher', 'relative_move', '1', false)
 end)
